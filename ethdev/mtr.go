08cddfc1ad0f8a286dfe12f6cd73652ec1008d08
suntianyuan <suntianyuan@baidu.com>
2024-06-13 15:26:30 +0800

    mtr fix

--- ethdev/mtr.go
+++ ethdev/mtr.go
@@ -62,7 +62,7 @@ static int add_mtr(uint16_t port, uint32_t mtr_id, uint32_t profile_id, uint32_t
 	memset(&params, 0, sizeof(params));
 	params.meter_profile_id = profile_id;
 	params.meter_policy_id = policy_id;
-	params.use_prev_mtr_color = 1;
+	params.use_prev_mtr_color = 0;
 	params.meter_enable = 1;
 	ret = rte_mtr_create(port, mtr_id, &params, 1, &error);
 	if (ret != 0) {
@@ -79,7 +79,6 @@ import (
 	"unsafe"
 
 	"github.com/tianyuansun/go-dpdk/common"
-	"github.com/yerden/go-dpdk/ethdev"
 )
 
 type MtrError C.struct_rte_mtr_error
@@ -92,30 +91,44 @@ func (e *MtrError) Unwrap() error {
 	return ErrorType(e._type)
 }
 
+// ErrorType is a type of an error.
+type ErrorType uint
+
+func (e ErrorType) Error() string {
+	if s, ok := errStr[e]; ok {
+		return s
+	}
+	return ""
+}
+
+var (
+	errStr = make(map[ErrorType]string)
+)
+
 func (e *MtrError) Cause() unsafe.Pointer {
 	return e.cause
 }
 
-func AddMeterProfile(port ethdev.Port, profileID uint32, cir, cbs, ebs uint64) error {
+func AddMeterProfile(port Port, profileID uint32, cir, cbs, ebs uint64) error {
 	return common.IntToErr(C.add_srtcm_mtr_profile(C.ushort(port), C.uint32_t(profileID), C.uint64_t(cir), C.uint64_t(cbs), C.uint64_t(ebs)))
 }
 
-func DeleteMeterProfile(port ethdev.Port, profileID uint32, mtrError *MtrError) error {
+func DeleteMeterProfile(port Port, profileID uint32, mtrError *MtrError) error {
 	return common.IntToErr(C.rte_mtr_meter_profile_delete(C.ushort(port), C.uint32_t(profileID), (*C.struct_rte_mtr_error)(mtrError)))
 }
 
-func AddMeterPolicy(port ethdev.Port, policyID uint32) error {
+func AddMeterPolicy(port Port, policyID uint32) error {
 	return common.IntToErr(C.add_mtr_policy(C.ushort(port), C.uint32_t(policyID)))
 }
 
-func DeleteMeterPolicy(port ethdev.Port, policyID uint32, mtrError *MtrError) error {
+func DeleteMeterPolicy(port Port, policyID uint32, mtrError *MtrError) error {
 	return common.IntToErr(C.rte_mtr_meter_policy_delete(C.ushort(port), C.uint32_t(policyID), (*C.struct_rte_mtr_error)(mtrError)))
 }
 
-func AddMtr(port ethdev.Port, mtrID uint32, profileID, policyID uint32) error {
+func AddMtr(port Port, mtrID uint32, profileID, policyID uint32) error {
 	return common.IntToErr(C.add_mtr(C.ushort(port), C.uint32_t(mtrID), C.uint32_t(profileID), C.uint32_t(policyID)))
 }
 
-func DeleteMtr(port ethdev.Port, mtrID uint32, mtrError *MtrError) error {
+func DeleteMtr(port Port, mtrID uint32, mtrError *MtrError) error {
 	return common.IntToErr(C.rte_mtr_destroy(C.ushort(port), C.uint32_t(mtrID), (*C.struct_rte_mtr_error)(mtrError)))
 }
